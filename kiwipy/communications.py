c343af3fc17936e463e8141962a0ffd29206aa13
Martin Uhrin <martin.uhrin@epfl.ch>
2018-04-02 10:26:49 +0200

    Git rid of local communicator

--- kiwipy/communications.py
+++ kiwipy/communications.py
@@ -4,7 +4,7 @@ import sys
 
 from . import futures
 
-__all__ = ['Communicator', 'CommunicatorHelper',
+__all__ = ['Communicator',
            'RemoteException', 'DeliveryFailed', 'TaskRejected', 'UnroutableError',
            'TimeoutError'
            ]
@@ -113,91 +113,3 @@ class Communicator(with_metaclass(abc.ABCMeta)):
     @abc.abstractmethod
     def await(self, future=None, timeout=None):
         pass
-
-
-class CommunicatorHelper(Communicator):
-    def __init__(self):
-        self._task_subscribers = []
-        self._broadcast_subscribers = []
-        self._rpc_subscribers = {}
-
-    def add_rpc_subscriber(self, subscriber, identifier):
-        self._rpc_subscribers[identifier] = subscriber
-
-    def remove_rpc_subscriber(self, identifier):
-        try:
-            self._rpc_subscribers.pop(identifier)
-        except KeyError:
-            raise ValueError("Unknown subscriber '{}'".format(identifier))
-
-    def add_task_subscriber(self, subscriber):
-        """
-        Register a task subscriber
-
-        :param subscriber: The task callback function
-        """
-        self._task_subscribers.append(subscriber)
-
-    def remove_task_subscriber(self, subscriber):
-        """
-        Deregister a task subscriber
-
-        :param subscriber: The task callback function
-        """
-        # TODO: Put exception guard and raise out own exception
-        self._task_subscribers.remove(subscriber)
-
-    def add_broadcast_subscriber(self, broadcast_subscriber):
-        self._broadcast_subscribers.append(broadcast_subscriber)
-
-    def remove_broadcast_subscriber(self, broadcast_subscriber):
-        self._broadcast_subscribers.remove(broadcast_subscriber)
-
-    def fire_task(self, msg):
-        future = futures.Future()
-        handled = False
-
-        for subscriber in self._task_subscribers:
-            try:
-                result = subscriber(msg)
-                if isinstance(result, futures.Future):
-                    future = result
-                else:
-                    future.set_result(result)
-
-                handled = True
-                break
-            except TaskRejected:
-                pass
-            except Exception:
-                future.set_exception(RemoteException(sys.exc_info()))
-                handled = True
-
-        if not handled:
-            future.set_exception(TaskRejected("Rejected by all subscribers"))
-
-        return future
-
-    def fire_rpc(self, recipient_id, msg):
-        try:
-            subscriber = self._rpc_subscribers[recipient_id]
-        except KeyError:
-            raise UnroutableError("Unknown rpc recipient '{}'".format(recipient_id))
-        else:
-            future = futures.Future()
-            try:
-                result = subscriber(msg)
-                if isinstance(result, futures.Future):
-                    futures.chain(result, future)
-                else:
-                    future.set_result(result)
-            except Exception:
-                future.set_exception(RemoteException(sys.exc_info()))
-            return future
-
-    def fire_broadcast(self, body, sender=None, subject=None, correlation_id=None):
-        for subscriber in self._broadcast_subscribers:
-            subscriber(body=body, sender=sender, subject=subject, correlation_id=correlation_id)
-        future = futures.Future()
-        future.set_result(True)
-        return future
--- test/test_local.py
+++
@@ -1,9 +0,0 @@
-import kiwipy
-import unittest
-
-from utils import CommunicatorTester
-
-
-class TestLocalCommunicator(CommunicatorTester, unittest.TestCase):
-    def create_communicator(self):
-        return kiwipy.LocalCommunicator()
